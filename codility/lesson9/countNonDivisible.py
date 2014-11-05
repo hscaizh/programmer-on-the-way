c4636c72f5f6b950af8cb400cbaf14413bc5d14b
hscaiz <hscaizh@gmail.com>
2014-11-05 15:38:22 +0800

    codility lession 9

---
+++ codility/lesson9/countNonDivisible.py
@@ -0,0 +1,49 @@
+#author:hscaizh
+#create on Nov 4, 2014
+
+'''
+题目:
+一个数组N个元素(可能有重复),对于其中的每一个数,求这个数组中不是此数的因子的元素的个数.
+*数的取值为[1,2*N]*
+
+思路:
+
+这道题想了很久,关键是没有看清题目条件的数的取值范围,一眼看去以为是2^N
+
+取值范围为[1,2*N]就很明朗了,先排序(避免重复数字多次计算,然后遍历排序后的数组B中记录[1,2N]之间是B[i]的倍数.
+
+复杂度分析:
+
+排序O(N*logN),记录个数 < O(sieve-of-eratosthenes(2N)) ~= O(NloglogN) < O(NlogN)
+故最终复杂度为O(NlogN),符合题目要求
+
+实现见代码.
+'''
+
+def solution(A):
+    N = len(A)
+    F = [0]*(2*N+1)
+    B = [x for x in A]
+    B.sort()
+    
+    i = 0
+    while i < N:
+        weight = 1
+        while i+1 < N and B[i+1] == B[i]:
+            weight += 1
+            i += 1
+        
+        k = B[i]
+        while k <=2*N:
+            F[k] += weight
+            k += B[i]
+        
+        i += 1
+    
+    retv = []
+    for x in A:
+        retv.append(N-F[x])
+    
+    return retv
+
+
---
+++ codility/lesson9/countSemiprimes.py
@@ -0,0 +1,40 @@
+#author:hscaizh
+#create on Nov 4, 2014
+
+'''
+题目:
+求M次,[p,q]区间内的满足条件为两个素数乘积的数的个数,p<=q<=N,时间复杂度要求为
+O(N*log(log(N))+M);
+
+
+思路:
+简单,用sieve-of-eratosthenes算法得到每个数的最小的因子,对每个数判断除去因子后的另一个数是否是素数,用F1记录到每个数时之前的数满足条件的数的个数.
+
+实现见代码.
+'''
+
+def solution(N, P, Q):
+    F, F1= [0]*(N+1), [0]*(N+1)
+    M = len(P)
+    
+    i = 2
+    while i*i <= N:
+        if F[i] == 0:
+            k = i*i
+            while k <= N:
+                F[k] = i
+                k += i
+        i += 1
+    
+    for i in xrange(4,N+1):
+        if F[i] != 0 and F[i/F[i]] ==0:
+            F1[i] = F1[i-1] + 1
+        else:
+            F1[i] = F1[i-1]
+    
+    retv = []
+    
+    for i in xrange(M):
+        retv.append(F1[Q[i]]-F1[P[i]-1])
+    
+    return retv
